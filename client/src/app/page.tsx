6be1a1e7a4ef39b5d79708bd81792ecf444ebc80
AYUSH WAGHALE <ayushwaghale777.com>
2025-10-25 14:53:50 +0530

    First Commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <h1 className="mt-4 text-4xl font-bold "> 
+        Welcome to Next.js with Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
