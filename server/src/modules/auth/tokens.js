d4ceced21cb94f4fe73b91c391ed52b80fa14339
gaurav0330 <gauravjikar070806@gmail.com>
2025-10-26 14:48:21 +0530

    login issue fixed

--- server/src/modules/auth/tokens.js
+++ server/src/modules/auth/tokens.js
@@ -1,18 +1,18 @@
 import jwt from "jsonwebtoken";
 
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const TOKEN_EXPIRY = process.env.TOKEN_EXPIRY || "15m";
 
 export function generateAccessToken(userId) {
-  return jwt.sign({ sub: userId }, ACCESS_TOKEN_SECRET, {
-    expiresIn: TOKEN_EXPIRY,
-  });
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  if (!secret) throw new Error("ACCESS_TOKEN_SECRET not set");
+
+  return jwt.sign({ sub: userId }, secret, { expiresIn: TOKEN_EXPIRY });
 }
 
 export function verifyAccessToken(token) {
   try {
-    return jwt.verify(token, ACCESS_TOKEN_SECRET);
-  } catch (err) {
+    return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  } catch {
     return null;
   }
 }
